718281eda1c3bb36695a0f77fd58c4c5b0cc5c58
Dayvson Lima <dayvsonlima31@gmail.com>
2020-08-10 23:42:14 -0300

    Remove unecessary files

--- backend/app/controllers/example_controller/index.go
+++
@@ -1,7 +0,0 @@
-package example_controller
-
-import "github.com/gin-gonic/gin"
-
-func Index(response *gin.Context) {
-	response.JSON(200, gin.H{"message": "this is one index page"})
-}
--- backend/app/controllers/posts/index.go
+++
@@ -1,7 +0,0 @@
-package posts_controller
-
-import "github.com/gin-gonic/gin"
-
-func Index(response *gin.Context) {
-	response.JSON(200, gin.H{"message": "this is one index page"})
-}
