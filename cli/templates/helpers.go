cc63e50394c3ef68d39d84e70fde3e728ca0b8e6
Dayvson Lima <dayvsonlima31@gmail.com>
2020-08-12 00:59:27 -0300

    Refect

--- cli/templates/helpers.go
+++ cli/templates/helpers.go
@@ -47,8 +47,9 @@ func Camelize(in string) string {
 }
 
 func Snakeze(str string) string {
-	var matchFirstCap = regexp.MustCompile("(.)([A-Z][a-z]+)")
-	var matchAllCap = regexp.MustCompile("([a-z0-9])([A-Z])")
+	matchFirstCap := regexp.MustCompile("(.)([A-Z][a-z]+)")
+	matchAllCap := regexp.MustCompile("([a-z0-9])([A-Z])")
+
 	snake := matchFirstCap.ReplaceAllString(str, "${1}_${2}")
 	snake = matchAllCap.ReplaceAllString(snake, "${1}_${2}")
 	return strings.ToLower(snake)
